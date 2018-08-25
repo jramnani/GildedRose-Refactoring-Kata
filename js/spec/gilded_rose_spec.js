5b76e8bba7da1e49297c4b988b5829a3571367a6
Jeff Ramnani <jeff.ramnani@teacherspayteachers.com>
2018-08-25 16:06:51 -0400

    Add tests and Conjured Item

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -24,4 +24,52 @@ describe("Gilded Rose", function() {
         expect(items[0].quality).toEqual(0);
     });
 
+    it("Aged Brie should go up in quality", function() {
+        const gildedRose = new Shop([ new Item("Aged Brie", 5, 10) ]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(11);
+    });
+
+    it("Sulfuras should stay the same", function() {
+        const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 5, 10) ]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(10);
+    });
+
+    it("Backstage passes go up in quality", function() {
+        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10) ]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(12);
+    });
+
+    it("Backstage passes go up in quality more closer to sell in", function() {
+        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) ]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(13);
+    });
+
+    it("Backstage passes go down in quality", function() {
+        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) ]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(0);
+    });
+
+    it("Conjured item", function() {
+        const gildedRose = new Shop([ new Item("Conjured item", 5, 10) ]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(8);
+    });
+
 });
