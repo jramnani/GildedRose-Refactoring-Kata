aac35c7fa1ebfde219de27786eb6e09670c21fa9
Jeff Ramnani <jeff.ramnani@teacherspayteachers.com>
2018-08-26 12:15:44 -0400

    Reorganize tests
    * Remove duplicate tests.
    * Organize the special cases alphabetically by Item class name.
    * Use the invariant tests (e.g. "quality never goes above 50") as
      integration tests.  This means we can delete the "Shop can handle"
      tests.

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,89 +1,5 @@
 describe("Gilded Rose", function() {
 
-    it("Normal items should degrade in quality by one over time", function() {
-        const gildedRose = new Shop([ new NormalItem("Munster Cheese", 5, 10) ]);
-
-        const items = gildedRose.updateQuality();
-
-        expect(items[0].quality).toEqual(9);
-    });
-
-    it("Normal items should degrade in quality by twice as much after the SellIn date", function() {
-        const gildedRose = new Shop([ new NormalItem("Munster Cheese", 0, 10) ]);
-
-        const items = gildedRose.updateQuality();
-
-        expect(items[0].quality).toEqual(8);
-    });
-
-    it("Normal item quality should not go below zero", function() {
-        const gildedRose = new Shop([ new Item("Munster Cheese", 0, 0) ]);
-
-        const items = gildedRose.updateQuality();
-
-        expect(items[0].quality).toEqual(0);
-    });
-
-    it("The quality of an item is never below zero", function() {
-       const gildedRose = new Shop([
-           new Item("Munster Cheese", 0, 0),
-           new AgedBrieItem(0, 0),
-           new ConjuredItem(0, 0),
-           new BackstagePassItem(0, 0),
-           new SulfurasItem(),
-       ]);
-
-       const items = gildedRose.updateQuality();
-
-       for (var i=0; i < items.length; i++) {
-           expect(items[i].quality).toBeGreaterThan(-1);
-       }
-    });
-
-    it("The quality of an item is never over 50", function() {
-        const gildedRose = new Shop([
-            new Item("Munster Cheese", 5, 50),
-            new AgedBrieItem(1, 50),
-            new BackstagePassItem(1, 50),
-        ]);
-
-        const items = gildedRose.updateQuality();
-
-        for (var i=0; i < items.length; i++) {
-            // console.log("DEBUG: item = " + items[i].name + " quality =" + items[i].quality);
-            expect(items[i].quality).toBeLessThan(51);
-        }
-    });
-
-    it("SulfurasItem should not change in quality", function () {
-        const item = new SulfurasItem();
-
-        item.updateSellIn();
-        item.updateQuality();
-
-        expect(item.quality).toEqual(80);
-    });
-
-    it("SulfurasItem never ages", function () {
-        const item = new SulfurasItem();
-        const sellInBefore = item.sellIn;
-
-        item.updateSellIn();
-        item.updateQuality();
-
-        const sellInAfter = item.sellIn;
-
-        expect(sellInBefore).toEqual(sellInAfter);
-    });
-
-    it("Shop can handle a SulfurasItem", function () {
-        const gildedRose = new Shop([ new SulfurasItem() ]);
-
-        const items = gildedRose.updateQuality();
-
-        expect(items[0].quality).toEqual(80);
-    });
-
     it("Normal items should decrease in quality by one each day", function () {
         const item = new NormalItem("Munster Cheese", 5, 10);
 
@@ -104,21 +20,36 @@ describe("Gilded Rose", function() {
         expect(item.quality).toEqual(8);
     });
 
-    it("Conjured items decrease in quality twice as fast as Normal items", function () {
-        const item = new ConjuredItem(5, 10);
+    it("The quality of an item is never below zero", function() {
+       const gildedRose = new Shop([
+           new Item("Munster Cheese", 0, 0),
+           new AgedBrieItem(0, 0),
+           new ConjuredItem(0, 0),
+           new BackstagePassItem(0, 0),
+           new SulfurasItem(),
+       ]);
 
-        item.updateSellIn();
-        item.updateQuality();
+       const items = gildedRose.updateQuality();
 
-        expect(item.quality).toEqual(8);
+       for (var i=0; i < items.length; i++) {
+           expect(items[i].quality).toBeGreaterThan(-1);
+       }
     });
 
-    it("Shop can handle ConjuredItems", function () {
-        const gildedRose = new Shop([ new ConjuredItem(5, 10)]);
+    it("The quality of an item is never over 50 (except Sulfuras)", function() {
+        const gildedRose = new Shop([
+            new Item("Munster Cheese", 5, 50),
+            new AgedBrieItem(1, 50),
+            new BackstagePassItem(1, 50),
+            new ConjuredItem(1, 50),
+        ]);
 
         const items = gildedRose.updateQuality();
 
-        expect(items[0].quality).toEqual(8)
+        for (var i=0; i < items.length; i++) {
+            // console.log("DEBUG: item = " + items[i].name + " quality =" + items[i].quality);
+            expect(items[i].quality).toBeLessThan(51);
+        }
     });
 
     it("Aged Brie quality increases over time", function () {
@@ -128,14 +59,6 @@ describe("Gilded Rose", function() {
         item.updateQuality();
 
         expect(item.quality).toBe(11);
-    });
-
-    it("Shop can handle Aged Brie", function () {
-        const gildedRose = new Shop([ new AgedBrieItem(5, 10) ]);
-
-        const items = gildedRose.updateQuality();
-
-        expect(items[0].quality).toEqual(11);
     });
 
     it("Backstage passes quality increases over time", function () {
@@ -174,11 +97,33 @@ describe("Gilded Rose", function() {
         expect(item.quality).toEqual(0);
     });
 
-    it("Shop can handle Backstage Passes", function () {
-        const gildedRose = new Shop([ new BackstagePassItem(20, 10) ]);
+    it("Conjured items decrease in quality twice as fast as Normal items", function () {
+        const item = new ConjuredItem(5, 10);
 
-        const items = gildedRose.updateQuality();
+        item.updateSellIn();
+        item.updateQuality();
 
-        expect(items[0].quality).toEqual(11);
-    })
+        expect(item.quality).toEqual(8);
+    });
+
+    it("SulfurasItem should not change in quality", function () {
+        const item = new SulfurasItem();
+
+        item.updateSellIn();
+        item.updateQuality();
+
+        expect(item.quality).toEqual(80);
+    });
+
+    it("SulfurasItem never ages", function () {
+        const item = new SulfurasItem();
+        const sellInBefore = item.sellIn;
+
+        item.updateSellIn();
+        item.updateQuality();
+
+        const sellInAfter = item.sellIn;
+
+        expect(sellInBefore).toEqual(sellInAfter);
+    });
 });
